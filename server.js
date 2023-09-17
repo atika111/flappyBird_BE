fcce3ed75b99892985b664da2b48488d58de1dbe
evgenyrinkevich <rinkevich2000@gmail.com>
2023-09-17 09:50:41 +0300

    serving images for FE

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 // Server Requires
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const run = require("./config/dbConn");
-const {globalErrorHandler} = require('./middleware/globalErrorHandler');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const run = require('./config/dbConn');
+const { globalErrorHandler } = require('./middleware/globalErrorHandler');
 
 // Global Middleware
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // Routes setup
-const authRoute = require("./routes/authRoutes");
-const scoresRoute = require("./routes/scoreRoutes");
-const usersRoute = require("./routes/userRoutes");
+const authRoute = require('./routes/authRoutes');
+const scoresRoute = require('./routes/scoreRoutes');
+const usersRoute = require('./routes/userRoutes');
 
-app.use("/auth", authRoute);
-app.use("/scores", scoresRoute);
-app.use("/users", usersRoute);
+app.use('/auth', authRoute);
+app.use('/scores', scoresRoute);
+app.use('/users', usersRoute);
 app.use(globalErrorHandler);
 
 // Server & MongoDB Connection
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   try {
-    run.dbConnection();
-    console.log("Server connected!");
+    // run.dbConnection();
+    console.log(`Server is running on port ${PORT}`);
   } catch (error) {
-    console.error("Error connecting to the database:", error);
+    console.error('Error connecting to the database:', error);
   }
 });
