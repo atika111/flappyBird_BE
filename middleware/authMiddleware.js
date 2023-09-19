76beac6c1328555221093d8cfb095a7f99dc8b6d
evgenyrinkevich <rinkevich2000@gmail.com>
2023-09-19 11:43:30 +0300

    fixed verifyToken

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,16 +1,15 @@
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const verifyToken = (req, res, next) => {
-  if (!req.headers.authorization) {
-    return res.status(401).send("Authorization headers required");
-  }
-  const token = req.headers.authorization.replace("Bearer ", "");
+  const cookies = req.cookies;
+  if (!cookies?.token) res.status(401).send('Token required.');
+  const token = cookies.token;
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).send("Invalid token");
+      return res.status(401).send('Invalid token');
     }
     req.body.userId = decoded.id;
   });
@@ -20,12 +19,12 @@ const verifyToken = (req, res, next) => {
 const passwordMatch = (req, res, next) => {
   const repPassword = req.body.repPassword;
   if (!repPassword)
-    return res.status(400).send("Please, provide repPassword field");
+    return res.status(400).send('Please, provide repPassword field');
   const isMatch = req.body.password !== repPassword;
   if (isMatch) {
     return res
       .status(400)
-      .send("Incorrect password. Please double-check your password.");
+      .send('Incorrect password. Please double-check your password.');
   }
   next();
 };
@@ -35,17 +34,17 @@ const verifyPassword = async (req, res, next) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
     if (!user) {
-      return res.status(400).send("User not found. Please log in again.");
+      return res.status(400).send('User not found. Please log in again.');
     }
 
     const isVerified = await bcrypt.compare(password, user.password);
     if (!isVerified) {
-      return res.status(400).send("Wrong password. Please try again.");
+      return res.status(400).send('Wrong password. Please try again.');
     }
     req.body.userId = user._id;
     next();
   } catch (error) {
-    console.log("error: ", error);
+    console.log('error: ', error);
   }
 };
 
