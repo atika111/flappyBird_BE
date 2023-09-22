060da5f02e234143eea41a4f32b03aada8ffdafb
evgenyrinkevich <rinkevich2000@gmail.com>
2023-09-22 14:38:57 +0300

    consistent errors on signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const User = require("../models/User");
+const User = require('../models/User');
 
 async function signUp(req, res) {
   try {
@@ -17,24 +17,26 @@ async function signUp(req, res) {
       nickname: user.nickname,
       picture: userImage,
     });
-    res.status(201).send("successfully signed up!");
+    res.status(201).send('successfully signed up!');
   } catch (error) {
     if (
       error.code === 11000 &&
       error.keyPattern &&
       error.keyPattern.email === 1
     ) {
-      return res.status(400).send({ error: "Email is already in use" });
+      return res.status(400).send({ error: 'Email is already in use' });
     }
-    if (error.name === "ValidationError") {
+    if (error.name === 'ValidationError') {
       const validationErrors = {};
       for (const field in error.errors) {
         validationErrors[field] = error.errors[field].message;
       }
-      res.status(400).send({ errors: validationErrors });
+      res
+        .status(400)
+        .send({ error: Object.values(validationErrors).join(',') });
     } else {
-      console.error("Error creating user:", error);
-      res.status(500).send({ error: "Internal server error" });
+      console.error('Error creating user:', error);
+      res.status(500).send({ error: 'Internal server error' });
     }
   }
 }
@@ -44,7 +46,7 @@ async function logIn(req, res) {
   try {
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: 'User not found' });
     }
 
     const userData = {
@@ -52,29 +54,29 @@ async function logIn(req, res) {
       userId: userId,
     };
     const token = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET);
-    res.cookie("token", token, { httpOnly: true });
+    res.cookie('token', token, { httpOnly: true });
 
     res.send({
       userId: user._id,
       nickname: user.nickname,
       email: user.email,
       admin: user.isAdmin,
-      picture: user.picture
+      picture: user.picture,
     });
   } catch (error) {
-    console.error("Error finding user by ID:", error);
-    res.status(500).json({ error: "Internal server error" });
+    console.error('Error finding user by ID:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 }
 
 function logOut(req, res) {
-  res.clearCookie("token");
-  res.send("Cookie has been deleted successfully");
+  res.clearCookie('token');
+  res.send('Cookie has been deleted successfully');
 }
 
 async function getUserByToken(req, res) {
   const cookies = req.cookies;
-  if (!cookies?.token) return res.status(401).send("Token required.");
+  if (!cookies?.token) return res.status(401).send('Token required.');
   const token = cookies.token;
 
   jwt.verify(
@@ -82,19 +84,19 @@ async function getUserByToken(req, res) {
     process.env.ACCESS_TOKEN_SECRET,
     async (err, decodedToken) => {
       if (err) {
-        return res.status(401).json({ message: "Unauthorized" });
+        return res.status(401).json({ message: 'Unauthorized' });
       }
       const user = await User.findById(decodedToken.userId);
 
       if (!user) {
-        return res.status(401).json({ message: "User not found" });
+        return res.status(401).json({ message: 'User not found' });
       }
 
       return res.send({
         nickname: user.nickname,
         userId: user._id,
         email: user.email,
-        admin: user.isAdmin
+        admin: user.isAdmin,
       });
     }
   );
